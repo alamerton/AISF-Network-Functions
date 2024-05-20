d29fa4be8bc23aa1ba16a414c549568ffeff22c4
Alfie Lamerton <alfielamerton@Alfies-MacBook-Pro.local>
2024-05-20 18:02:30 +0100

    Playing with tokeniser bugs

--- neural_test.py
+++ neural_test.py
@@ -3,34 +3,69 @@ import torch
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 from torch.utils.data import DataLoader, TensorDataset, random_split
 from sklearn.model_selection import train_test_split
+from transformers import Trainer
+from datasets import load_dataset
+from transformers import TrainingArguments
+from datasets import load_metric
 
 # %%
-# Load the pre-trained BERT model and tokenizer.
-tokeniser = AutoTokenizer.from_pretrained('bert-base-uncased')
 model = AutoModelForSequenceClassification.from_pretrained('bert-base-uncased', num_labels=2)
+tokeniser = AutoTokenizer.from_pretrained('bert-base-uncased')
 
 #%%
-from datasets import load_dataset
-
 dataset = load_dataset("Alamerton/Synthetic-Palindromes")
 
 #%%
-# Tokenize data
-def tokenize_function(examples):
-    return tokeniser(examples["text"], padding="max_length", truncation=True, max_length=512)
+tokenised_dataset = dataset.map(lambda examples: tokeniser(examples['train'], padding='max_length', truncation=True), batched=True)
 
-tokenised_dataset = dataset.map(tokenize_function, batched=True)
-tokenised_dataset = tokenised_dataset.remove_columns(["text"])
-# tokenised_dataset = tokenised_dataset.set_format('torch')
+# %%
+train_dataset, eval_dataset = train_test_split(tokenised_dataset, test_size=0.2, random_state=42)
 
-print(tokenised_dataset)
-    
-dataloader = DataLoader(tokenised_dataset["train"], batch_size=32, shuffle=True)
+# %%
+metric = load_metric("metric_name")
 
-print(dataloader)
+def compute_metrics(eval_preds):
+    predictions, labels = eval_preds
+    return metric.compute(predictions=predictions, references=labels)
+
+# %%
+
+# def compute_metrics(pred):
+#     labels = pred.label_ids
+#     preds = pred.predictions.argmax(-1)
+#     precision, recall, f1, _ = precision_recall_fscore_support(labels, preds, average='macro')
+#     acc = accuracy_score(labels, preds)
+#     return {
+#         'accuracy': acc,
+#         'f1': f1,
+#         'precision': precision,
+#         'recall': recall,
+#     }
+
 #%%
-model.train()
+training_args = TrainingArguments(
+    evaluation_strategy='epoch',
+    logging_strategy='epoch',
+    learning_rate='1e-4',
+    num_train_epochs=3,
+    per_device_train_batch_size=16,
+    per_device_eval_batch_size=16,
+    output_dir='./results',
+    overwrite_output_dir=True,
+    remove_unused_columns=False,
+    save_total_limit=1
+)
 
-model.eval()
 
+# %%
+
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=train_dataset,
+    eval_dataset=eval_dataset,
+    compute_metrics=compute_metrics,
+)
 print("Training complete.")
+
+# %%
